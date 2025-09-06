07e2e345fdb2aa75b888e4e583a92f1510713749
zaforsadiq987@gmail.com <138440583+AbuZafor99@users.noreply.github.com>
2025-09-06 20:52:21 +0600

    add flutter crushlytics config

---
+++ lib/app/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CraftyBay extends StatelessWidget {
+  const CraftyBay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
